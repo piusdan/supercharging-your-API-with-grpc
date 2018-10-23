7ad358dc9a51f42d9f489e1ec74972c23d05896b
Pius Dan <npiusdan@gmail.com>
2018-10-24 00:07:43 +0300

    Auth using interceptors

--- auth/client.py
+++ auth/client.py
@@ -1,13 +1,11 @@
 import grpc
 import authentication_example_pb2, authentication_example_pb2_grpc
+import header_manipulator_client_interceptor
+
 
 def say_hello(stub):
     greeting = authentication_example_pb2.HelloRequest(greeting="Hello")
-    metadata = [
-            ('client', 'localhost-client'),
-            ('apikey', 'myapikey')
-        ]
-    resp = stub.SayHello(greeting, metadata=metadata)
+    resp = stub.SayHello(greeting)
     print(resp)
 
 
@@ -15,14 +13,16 @@ def main():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = authentication_example_pb2_grpc.HelloServiceStub(channel)
+    header_adder_interceptor = header_manipulator_client_interceptor.header_adder_interceptor(
+        "api_key", "42"
+    )
+    with grpc.insecure_channel("localhost:50051") as channel:
+        intercept_channel = grpc.intercept_channel(channel, header_adder_interceptor)
+        stub = authentication_example_pb2_grpc.HelloServiceStub(intercept_channel)
         print("-------------- SayHello --------------")
-        
         say_hello(stub)
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
--- auth/server.py
+++ auth/server.py
@@ -2,6 +2,8 @@ import time
 import grpc
 from concurrent import futures
 
+from request_header_validator_interceptor import RequestHeaderValidatorInterceptor
+
 import authentication_example_pb2, authentication_example_pb2_grpc
 
 
@@ -13,12 +15,20 @@ class HelloWorldServicer(authentication_example_pb2_grpc.HelloServiceServicer):
 
     def SayHello(self, request, context):
         print(request)
-        print("received: {}".format(greeting))
-        return authentication_example_pb2.HelloResponse(reply="{} to you".format(request.greeting))
+        print("received: {}".format(request))
+        return authentication_example_pb2.HelloResponse(
+            reply="{} to you".format(request.greeting)
+        )
 
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    header_validator = RequestHeaderValidatorInterceptor(
+        "api_key", "42", grpc.StatusCode.UNAUTHENTICATED, "Access denied!"
+    )
+
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10), interceptors=(header_validator,)
+    )
     authentication_example_pb2_grpc.add_HelloServiceServicer_to_server(
         HelloWorldServicer(), server
     )
